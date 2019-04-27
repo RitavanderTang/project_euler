afa22fe425aacbde0f3ae245e3647bbf7d0c3f14
RitavanderTang <ritavandertang@gmail.com>
2019-04-27 06:45:30 +0000

    Project Euler 03 done

--- project_euler_03.py
+++ project_euler_03.py
@@ -4,21 +4,40 @@
 
 # What is the largest prime factor of the number 600851475143 ?
 
-list_1 = list(range(3, 1000))
-prime_num = [2]
-for i in list_1:
-  num = 2
-  while i >= num:
-    if i == num:
-      prime_num.append(i)
-      break
+def calc_prime_num(max_num):
+  input_range = list(range(3, (max_num + 1)))
+  prime_num = [2]
+  for i in input_range:
+    num = 2
+    while i >= num:
+      if i == num:
+        prime_num.append(i)
+        break
 
-    uitkomst = i / num
-    if uitkomst.is_integer():
-      break
-    elif not uitkomst.is_integer():
-      num += 1
+      uitkomst = i / num
+      if uitkomst.is_integer():
+        break
+      elif not uitkomst.is_integer():
+        num += 1
+  return prime_num
+
+def calc_prime_factors(number, prime_numbers):
+  factors = []
+  for prime in prime_numbers:
+    while number.is_integer():
+      uitkomst = number / prime
+      if uitkomst.is_integer():
+        factors.append(prime)
+        number = uitkomst
+        if uitkomst == 1.0:
+          print(factors)
+          exit()
+      elif not uitkomst.is_integer():
+        break
+
+prime_numbers = calc_prime_num(10000)
+number = float(600851475143)
+calc_prime_factors(number, prime_numbers)
 
 
-print(prime_num)
 
